a77db44e73a461c8196efe625c95c114d4ba7367
Alvaro Vilela <worrlock@gmail.com>
2023-09-04 11:19:10 -0300

    first commit

---
+++ day-1/Começo.py
@@ -0,0 +1,20 @@
+# print("Hello " + input("what is your name?"))
+
+# str = input("What is your name?")
+# print(len(str))
+
+# a = input("Escreva o valor de A: ")
+# b = input("Escreva o valor de B: ")
+
+# c = a
+# a = b
+# print("a = " + a)
+# b = c
+# print("b = " + b)
+
+print("#1. Create a greeting for your program.")
+cidade = input("What's the name of the city you grew up in?")
+
+pet = input("What's your pet's name?")
+
+print("Your band name could be " + cidade + " " + pet)
